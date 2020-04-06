c1fced0ae088320582f8e450dc456efaa2a049b3
Giscomaps <63242716+Giscomaps@users.noreply.github.com>
2020-04-06 09:10:58 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1s631dO4dLTggD7svAXN40wBCYng2CbEz'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['van', 'moto', 'camion', 'automovil', 'peaton', 'bus']
 path = Path(__file__).parent
 
 app = Starlette()
